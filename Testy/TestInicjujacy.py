035ba52f750d07d0d3640e7fc3813ed898a2bc6f
Jakub Olejniczak <jakubkuba997@gmail.com>
2019-06-13 18:20:36 +0200

    General restructarization of project

--- Testy/TestInicjujacy.py
+++ Testy/TestInicjujacy.py
@@ -1,2 +1,21 @@
+import unittest
 
 
+class TestInicjujacyTestCases(unittest.TestCase):
+    def setUp(self) -> None:
+        import FunctionGlobal   # unknown reason for showing error - IDE bug
+        self.function_global = FunctionGlobal
+        self.test_id = "TS001"
+
+    def test_inicjujacy(self):
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+
+# use only for debug
+if __name__ == '__main__':
+    import FunctionGlobal
+    function_global = FunctionGlobal
+    print(function_global.Constants().pages_bytes_name)
