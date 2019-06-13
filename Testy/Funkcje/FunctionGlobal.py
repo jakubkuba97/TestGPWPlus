ce9592052d51f8133701c2b823d46ffba4632962
Jakub Olejniczak <jakubkuba997@gmail.com>
2019-06-13 02:15:37 +0200

    Updated FunctionGlobal with basic functions

--- Testy/Funkcje/FunctionGlobal.py
+++ Testy/Funkcje/FunctionGlobal.py
@@ -1,17 +1,35 @@
-
+from subprocess import Popen
 
 
 class Constants:
-    pass
+    @staticmethod
+    def get_path_to_program() -> str:
+        from os import path
+        path_to_project = path.dirname(path.dirname(path.dirname(path.realpath(__file__))))
+        path_to_program = (path_to_project + r"\Dokumentacja\Przedmiot testow")
+        return path_to_program
 
 
 class ForSetUp:
-    pass
+    @staticmethod
+    def launch_program() -> Popen:
+        from subprocess import PIPE, STDOUT
+        path_to_program = Constants().get_path_to_program()
+        the_main_process = Popen(['GPWPlus.exe', '-show_streams', path_to_program], shell=True, stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        return the_main_process
 
 
 class ForTearDown:
-    pass
+    @staticmethod
+    def close_program(the_process: Popen):
+        the_process.terminate()
 
 
 class CommonFunctions:
     pass
+
+
+# use only for debug
+if __name__ == '__main__':
+    x = ForSetUp().launch_program()
+    ForTearDown().close_program(x)
